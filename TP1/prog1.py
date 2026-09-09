eeb570b7e39162ad0e140a4ff36b02c85d6517a7
divine20062006 <oubourradivine27@gmail.com>
2026-09-09 15:53:16 +0200

    tp fonction

--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,1 +1,11 @@
+
 print("hello, world!")
+
+while True:
+    a = input("Entrez un nombre (ou CTRL-C pour quitter) : ")
+    b = input("Entrez un autre nombre (ou CTRL-C pour quitter) : ")
+    nombreA = float(a)
+    nombreB = float(b)
+
+    resultat = nombreA ** nombreB
+    print (f"Le résultat de {nombreA} élevé à la puissance {nombreB} est : {resultat}")
