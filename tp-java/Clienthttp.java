7aa09e51558c25ed9e570d69c5000ecd0286beb0
divine20062006 <oubourradivine27@gmail.com>
2026-09-21 14:02:03 +0200

    TP Termine : Partie 4 Client HTTP

---
+++ tp-java/Clienthttp.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.net.*;
+
+public class Clienthttp {
+    public static void main(String[] args) {
+        try {
+            // Ouverture du socket sur le nom d'hote (args[0]) et le port 80 (HTTP)
+            Socket socket = new Socket(args[0], 80);
+
+            OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+            InputStreamReader isw = new InputStreamReader(socket.getInputStream());
+
+            BufferedWriter bufOut = new BufferedWriter(osw);
+            BufferedReader bufIn = new BufferedReader(isw);
+
+            String request = "GET / HTTP/1.0\r\n\r\n"; // requete HTTP
+            bufOut.write(request, 0, request.length());
+            bufOut.flush();
+
+            String line = bufIn.readLine(); // lecture ligne par ligne
+            while (line != null) {          // tant qu'il y a des donnees recues,
+                System.out.println(line);   // ... les afficher
+                line = bufIn.readLine();
+            }
+
+            bufIn.close();
+            bufOut.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
